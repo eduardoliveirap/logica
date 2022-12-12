7381cb6ea7e4504f656d114b1d0c9f731648b699
eduardoliveirap <eduardoliveirap1@hotmail.com>
2022-12-12 10:37:26 -0300

    commit:learning js

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello World")
+console.log(2*3)
+console.log(2+3)
+
+let nome = "Eduardo Pinheiro";
+const dataNascimento = "27/02/2004"
+
+document.write("<h1>Hello world</h1> ");
+document.write(`<h2>Nascido em: ${dataNascimento}</h2>`)
+document.getElementsByTagName("h1")[0].style.color = "red";
+document.write(nome);
+document.write("<br>")
+nome = "Suelen"
+document.write(nome);
+document.write(dataNascimento);
